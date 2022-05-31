cf321b4cfd7d6459fd170751b559531ced13130d
Dddarknight <9801677@gmail.com>
2022-05-31 18:37:25 +0300

    step8

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -25,20 +25,19 @@ def find_path(path, node):
     return node
 
 
-def plain_elem(value, file1, file2, key, path):
+def plain_elem(value, file1, file2, path_int):
     dict_diff = {'updated': ' was updated. From ',
                  'removed': ' was removed',
                  'added': ' was added with value: '}
-    new_path = path + key
-    result_str = f"Property '{new_path}'"
+    result_str = f"Property '{path_int}'"
     if value == 'removed':
         result_str += f"{dict_diff[value]}\n"
     elif value == 'updated':
-        fact_value = normalize1(normalize2(find_path(new_path, file2)))
-        old_value = normalize1(normalize2(find_path(new_path, file1)))
+        fact_value = normalize1(normalize2(find_path(path_int, file2)))
+        old_value = normalize1(normalize2(find_path(path_int, file1)))
         result_str += f"{dict_diff[value]}{old_value} to {fact_value}\n"
     elif value == 'added':
-        fact_value = normalize1(normalize2(find_path(new_path, file2)))
+        fact_value = normalize1(normalize2(find_path(path_int, file2)))
         result_str += f"{dict_diff[value]}{fact_value}\n"
     return result_str
 
@@ -47,12 +46,11 @@ def plain(tree, file1, file2):
 
     def inner(tree, path='', result_str=''):
         for key, value in tree.items():
+            path_int = f'{path}{key}'
             if isinstance(value, dict):
-                path_int = f'{path}{key}.'
-                result_str += inner(value, path=path_int)
+                result_str += inner(value, path=(path_int+'.'))
             if value in ('updated', 'added', 'removed'):
-                result_str += plain_elem(value, file1, file2, key, path)
+                result_str += plain_elem(value, file1, file2, path_int)
         path = ''
         return result_str
-    result = (inner(tree)).strip('\n')
-    return result
+    return (inner(tree)).strip('\n')
